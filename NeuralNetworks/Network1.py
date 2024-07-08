b9e8f6a386221ae671605a626fa66b7d44aed327
Erik Kaufman <kaufmane558@gmail.com>
2024-07-08 16:27:07 -0500

    Began laying out steps

---
+++ NeuralNetworks/Network1.py
@@ -0,0 +1,18 @@
+
+# Erik Kaufman
+# 
+# Goal: Create and train a neural network that processes 28x28 black and white 
+# pixel images to detect whitch number (0-9) they are.
+#
+# I also want to be able to write the weights into a seperate file so I don't 
+# have to retrain the model each time I want to test it out
+
+# Setup the Neural Netowrk
+
+# create layers of nodes
+
+# Initilize weights and biasies to random
+
+# Load MNIST Data set
+
+# Train using backpropogation
